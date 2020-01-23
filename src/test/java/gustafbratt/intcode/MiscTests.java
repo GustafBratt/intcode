be663aaf3d9b0492750c61e47f8857aa6d69e24b
Gustaf Bratt <gustaf.bratt@miniumtech.com>
2020-01-23 21:06:27 +0100

    Small cleanup

--- src/test/java/gustafbratt/intcode/MiscTests.java
+++ src/test/java/gustafbratt/intcode/MiscTests.java
@@ -15,4 +15,34 @@ public class MiscTests {
         c.printMemoryDump();
         assertThat(c.memory.get(BigInteger.valueOf(10))).isEqualTo(BigInteger.TWO);
     }
+
+    @Test
+    public void testIO(){
+        InputOutput io = new InputOutput();
+        assertThat(io.isEmpty()).isTrue();
+        io.write(7);
+        io.write(8);
+        io.write(9);
+        assertThat(io.isEmpty()).isFalse();
+        assertThat(io.toString()).isEqualTo("7,8,9");
+        assertThat(io.read()).isEqualTo(7);
+        assertThat(io.read()).isEqualTo(8);
+        assertThat(io.read()).isEqualTo(9);
+        assertThat(io.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void blockingInupt() {
+        String program = "3,3,99,0";
+        InputOutput in = new InputOutput();
+        Computer c = new Computer(program, in, new InputOutput());
+        c.printMemoryDump();
+        State state = c.run();
+        assertThat(state).isEqualTo(State.BLOCKED);
+        in.write(242);
+        state = c.run();
+        assertThat(state).isEqualTo(State.ENDED);
+        c.printMemoryDump();
+        assertThat(c.getMemCell(BigInteger.valueOf(3))).isEqualTo(BigInteger.valueOf(242));
+    }
 }
